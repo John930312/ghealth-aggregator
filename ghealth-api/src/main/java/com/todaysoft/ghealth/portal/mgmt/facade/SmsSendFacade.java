898af0e1f29685286ae20efc0841eb694b978919
John <ljl845851667@163.com>
2018-09-17 14:41:13 +0800

    sms调整

--- ghealth-api/src/main/java/com/todaysoft/ghealth/portal/mgmt/facade/SmsSendFacade.java
+++ ghealth-api/src/main/java/com/todaysoft/ghealth/portal/mgmt/facade/SmsSendFacade.java
@@ -21,10 +21,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import static java.util.stream.Collectors.toList;
 
@@ -117,14 +114,19 @@ public class SmsSendFacade
         if (StringUtils.isNotEmpty(request.getAgencyId()))
         {
             List<String> agencyIds = Arrays.asList(request.getAgencyId().split(","));
+            List<String> allCustomerPhones  = new ArrayList<String>();
             agencyIds.forEach(x -> {
                 customerSearcher.setAgencyId(x);
                 List<Customer> customerList = customerService.list(customerSearcher);
                 smsSend.setAgencyId("1");
                 customerList.forEach(c -> {
-                    smsSend.setId(IdGen.uuid());
-                    smsSend.setPhone(c.getPhone());
-                    sendService.create(smsSend);
+                    if (!allCustomerPhones.contains(c.getPhone()))
+                    {
+                        smsSend.setId(IdGen.uuid());
+                        smsSend.setPhone(c.getPhone());
+                        sendService.create(smsSend);
+                        allCustomerPhones.add(c.getPhone());
+                    }
                 });
             });
         }
